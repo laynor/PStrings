40384c75f4bc6fbfd782d62ee659339778009909
Alessandro Piras <laynor@gmail.com>
2012-04-07 16:15:44 +0200

    Added pstrings-xlib system and function documentation on xlib.lisp

---
+++ pstrings-xlib.asd
@@ -0,0 +1,6 @@
+(asdf:defsystem "pstrings-xlib"
+  :description "Tests for the pstring library"
+  :author "Alessandro Piras <laynor@gmail.com>"
+  :version "0.1"
+  :depends-on (:pstrings :clx)
+  :components ((:file "xlib")))
--- xlib.lisp
+++ xlib.lisp
@@ -6,6 +6,22 @@
 			 (resolution-x "*") (resolution-y "*") (spacing "*")
 			 (average-width "*") (registry "*") (encoding "*")
 			 &allow-other-keys)
+  "Builds a font name from its key arguments.
+The argument N specifies how many parameters to ignore, starting from the last, in this order:
+	    foundry
+	    family
+	    weight
+	    slant
+	    width
+	    style
+	    pixel-size
+	    point-size
+	    resolution-x
+	    resolution-y
+	    spacing
+	    average-width
+	    registry
+	    encoding"
   (declare (ignorable foundry family weight slant width style pixel-size point-size
 		      resolution-x resolution-y spacing average-width registry encoding))
   (format nil "~{-~a~}"
@@ -18,20 +34,45 @@
 	  
   
 (defun xlib-face-font-name (face &optional (n 0))
+  "Returns the font name used to display text with FACE. 
+The optional argument N specifies how many parameters to ignore,
+starting from the last, in this order:
+	    foundry
+	    family
+	    weight
+	    slant
+	    width
+	    style
+	    pixel-size
+	    point-size
+	    resolution-x
+	    resolution-y
+	    spacing
+	    average-width
+	    registry
+	    encoding"
   (apply #'xlib-font-name n (hash-table-plist face)))
 
 
 (defun xlib-face-color (colormap face property)
+  "Returns a CLX color object built from a PROPERTY of FACE, given a COLORMAP"
   (ignore-errors
     (xlib:alloc-color colormap (face-property face property))))
 
 (defun xlib-face-foreground (colormap face)
+  "Returns the foreground color of FACE, given COLORMAP, as a CLX color object."
   (xlib-face-color colormap face :foreground))
 
 (defun xlib-face-background (colormap face)
+  "Returns the background color of FACE, given COLORMAP, as a CLX color object."
   (xlib-face-color colormap face :background))
 
+;;; Default font solver
 (defun xlib-face-simple-resolve-font (display face &optional (n 0))
+  "Returns the font name used to display text propertized with FACE.
+The font name is resolved incrementally ignoring the font properties,
+starting from the full specification and ending with
+-*-*-*-*-*-*-*-*-*-*-*-*-*-* if no matching font can be found."
   (unless (> n 14)
     (handler-case (xlib:open-font
 		   display
@@ -41,35 +82,46 @@
 (defvar *xlib-face-font-solver* #'xlib-face-simple-resolve-font
   "Function used by the xlib pstring routines for face->font resolution.")
 
-(defun xlib-face-font-select (context face)
+(defun xlib-face-font-select (gcontext face)
+  "Returns the name of the font used to display text propertized with
+FACE using the graphic context GCONTEXT, using
+`*xlib-face-font-solver*' to resolve the font name."
   (funcall *xlib-face-font-solver*
-	   (xlib:gcontext-display context)
+	   (xlib:gcontext-display gcontext)
 	   face))
 
-(defun xlib-face-context-attributes (context colormap face)
-  (let ((foreground `(and ,colormap (xlib-face-foreground ,colormap ,face)))
-	(background `(and ,colormap (xlib-face-background ,colormap ,face)))
-	(font `(xlib-face-font-select ,context ,face)))
-    (list :foreground foreground :background background :font font)))
+;;; Helper for the xlib-with-face-attributes macro
+(eval-when (:compile-toplevel)
+  (defun xlib-face-gcontext-attributes (gcontext colormap face)
+    (let ((foreground `(and ,colormap (xlib-face-foreground ,colormap ,face)))
+	  (background `(and ,colormap (xlib-face-background ,colormap ,face)))
+	  (font `(xlib-face-font-select ,gcontext ,face)))
+      (list :foreground foreground :background background :font font))))
 
-(defmacro xlib-with-face-attributes ((face context colormap) &body body)
-  (once-only (face context colormap)
+(defmacro xlib-with-face-attributes ((face gcontext colormap) &body body)
+  "Sets up GCONTEXT so that text drawn with xlib:draw-image-glyphs and
+  xlib:draw-glyphs in BODY is printed according to FACE's attributes."
+  (once-only (face gcontext colormap)
     (with-gensyms (f)
       `(let ((,f (and ,face (merge-face-ancestors (copy-face ,face)))))
-	 (xlib:with-gcontext (,context ,@(xlib-face-context-attributes context colormap f))
+	 (xlib:with-gcontext (,gcontext ,@(xlib-face-gcontext-attributes gcontext colormap f))
 	   ,@body)))))
     
-(defun xlib-draw-pstring (ss window context x y &optional draw-image-glyphs-p)
-  (pstring-do-slices (ss sub props)
+(defun xlib-draw-pstring (pstring window gcontext x y &optional draw-image-glyphs-p)
+  "Draws PSTRING on WINDOW at coordinates X, Y using GCONTEXT.
+The optional argument DRAW-IMAGE-GLYPHS-P controls whether the text is
+drawn using `xlib:draw-glyphs' (default) or `xlib:draw-image-glyphs'."
+  (pstring-do-slices (pstring sub props)
     (let* ((fname (getf props :face))
 	   (face (lookup-face (or fname :default))))
-      (xlib-with-face-attributes (face context (xlib:window-colormap window))
+      (xlib-with-face-attributes (face gcontext (xlib:window-colormap window))
 	(if draw-image-glyphs-p
-	    (xlib:draw-image-glyphs window context x y sub)
-	    (xlib:draw-glyphs window context x y sub))
-	(incf x (xlib:text-extents context sub))))))
+	    (xlib:draw-image-glyphs window gcontext x y sub)
+	    (xlib:draw-glyphs window gcontext x y sub))
+	(incf x (xlib:text-extents gcontext sub))))))
 
 (defun xlib-pstring-extents (gcontext pstring)
+  "Returns the width, height and ascent of PSTRING, as it would be drawn using GCONTEXT."
   (let ((width 0) (height 0) (ascent 0) left (right 0))
     (pstring-do-slices (pstring sub props)
       (let* ((fname (getf props :face))
@@ -85,5 +137,6 @@
 	    (setf height (max height (+ fa fd)))))))
     (values width height ascent )))
 
+;;; EXPORTS
 (export 'xlib-draw-pstring)
 (export 'xlib-pstring-extents)
